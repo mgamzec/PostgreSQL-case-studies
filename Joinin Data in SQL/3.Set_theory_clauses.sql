a54eca2782691c881a6742aa85a8b41622e29a82
Merve Gamze Çınar <62151645+mgamzec@users.noreply.github.com>
2023-07-11 21:33:24 +0300

    Set_theory_clauses.sql

--- Joinin Data in SQL/3.Set_theory_clauses.sql
+++ Joinin Data in SQL/3.Set_theory_clauses.sql
@@ -38,17 +38,29 @@ As you saw, duplicates were removed from the previous two exercises by using UNI
 To include duplicates, you can use UNION ALL.
 */
 
--- Select fields
+/* 
+Perform an appropriate set operation that determines all pairs of country code and year (in that order) from economies and populations, excluding duplicates.
+Order by country code and year.
+*/
+
+-- Query that determines all pairs of code and year from economies and populations, without duplicates
 SELECT code, year
-  -- From economies
-  FROM economies
-	-- Set theory clause
-	UNION ALL
--- Select fields
+FROM economies
+UNION 
 SELECT country_code, year
-  -- From populations
-  FROM populations
--- Order by code, year
+FROM populations
+ORDER BY code, year;
+
+/* 
+Amend the query to return all combinations (including duplicates) of country code and year in the economies or the populations tables.
+*/
+	
+SELECT code, year
+FROM economies
+-- Set theory clause
+UNION ALL
+SELECT country_code, year
+FROM populations
 ORDER BY code, year;
 
 -----
